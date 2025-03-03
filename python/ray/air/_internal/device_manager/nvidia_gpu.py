705a10d96d391259070d0bda0d2ad35a18ac9603
huafengchun <huafengchun@gmail.com>
2025-03-03 11:43:24 +0000

    Use torch.Stream instead of cupy.ExternalStream
    Signed-off-by: hipudding <huafengchun@gmail.com>

--- python/ray/air/_internal/device_manager/nvidia_gpu.py
+++ python/ray/air/_internal/device_manager/nvidia_gpu.py
@@ -77,3 +77,11 @@ class CUDATorchDeviceManager(TorchDeviceManager):
     def get_current_stream(self) -> torch.cuda.Stream:
         """Get current stream for cuda device"""
         return torch.cuda.current_stream()
+
+    def create_event(self) -> torch.cuda.Event:
+        """Create a event on cuda device"""
+        return torch.cuda.Event()
+
+    def get_device_context(self, device: torch.device) -> torch.cuda.device:
+        """Get a device context for cuda device"""
+        return torch.cuda.device(device)
